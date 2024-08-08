5ab5ad87347aefa60dc13411c6a322a2de6fec40
Alexis Aparicio <alexiiz360@gmail.com>
2024-08-07 19:49:43 -0500

    updates

--- webpage/frontend/src/pages/LandingPage.js
+++ webpage/frontend/src/pages/LandingPage.js
@@ -5,7 +5,7 @@ const LandingPage = () => {
   return (
     <div className="landing-container">
       <h1>Bienvenido a Global Mind Solutions</h1>
-      <img src="https://www.globalmindsolutions.com/wp-content/uploads/2021/06/Global-Mind-Solutions-Logo-1.png" alt="Global Mind Solutions" />
+      <img src="https://globalmind-solutions.com/wp-content/uploads/2023/07/GMS-LOGO-sin-fondo-03.png" alt="Global Mind Solutions" />
       <a href="/login">Iniciar Sesión</a>
     </div>
   );
